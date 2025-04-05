052bae625cd61432d129eeb3656730135d489795
Candice Bell <candicebell.learninghub@gmail.com>
2025-04-05 14:48:00 -0400

    Added Promises/Async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -69,4 +69,77 @@ public_users.get('/review/:isbn',function (req, res) {
   }
 });
 
+//Task 10 - Add the code for getting the list of books available in the shop (done in Task 1) using Promise callbacks or async-await with Axios
+const getBookList = () => {
+    return new Promise((resolve,reject)=>{
+      resolve(books);
+  })
+}
+
+// Get the book list available in the shop
+public_users.get('/', (req, res) => {
+    getBookList().then(
+      (bk)=>res.send(JSON.stringify(bk, null, 4)),
+      (error) => res.send("denied")
+  );  
+});
+
+//Task 11 - Add the code for getting the book details based on ISBN (done in Task 2) using Promise callbacks or async-await with Axios
+const getFromISBN = (isbn) => {
+    let book_ = books[isbn];  
+    return new Promise((resolve,reject)=>{
+      if (book_) {
+        resolve(book_);
+      }else{
+        reject("Unable to find book!");
+    }    
+  })
+}
+
+//Task 12 - Add the code for getting the book details based on Author (done in Task 3) using Promise callbacks or async-await with Axios
+const getFromAuthor = (author) => {
+    let output = [];
+    return new Promise((resolve,reject)=>{
+      for (let isbn in books) {
+        let book_ = books[isbn];
+        if (book_.author === author){
+          output.push(book_);
+        }
+      }
+      resolve(output);  
+  })
+}  
+
+// Get book details based on author
+public_users.get('/author/:author', (req, res) => {
+    const author = req.params.author;
+    getFromAuthor(author)
+    .then(
+      result =>res.send(JSON.stringify(result, null, 4))
+  );
+});
+
+//Task 13 - Add the code for getting the book details based on Title (done in Task 4) using Promise callbacks or async-await with Axios
+const getFromTitle = (title) => {
+    let output = [];
+    return new Promise((resolve,reject)=>{
+      for (let isbn in books) {
+        let book_ = books[isbn];
+        if (book_.title === title){
+          output.push(book_);
+        }
+      }
+      resolve(output);  
+  })
+}
+
+// Get all books based on title
+public_users.get('/title/:title',(req, res) => {
+    const title = req.params.title;
+    getFromTitle(title)
+    .then(
+      result =>res.send(JSON.stringify(result, null, 4))
+  );
+});
+
 module.exports.general = public_users;
